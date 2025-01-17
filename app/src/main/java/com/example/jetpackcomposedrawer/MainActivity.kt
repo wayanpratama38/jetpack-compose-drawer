a98682d1229c8983e5ed97b53c3be05cbc82a983
wynprtm <wayanpratama38@gmail.com>
2025-01-18 00:02:15 +0700

    Handling Drawer State

--- app/src/main/java/com/example/jetpackcomposedrawer/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposedrawer/MainActivity.kt
@@ -5,24 +5,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposedrawer.ui.theme.JetpackComposeDrawerTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,21 +53,48 @@ class MainActivity : ComponentActivity() {
 fun NavigationDrawer(
     modifier : Modifier = Modifier
 ){
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
     Scaffold (
         topBar = {
             MyTopBar(
                 onMenuClick = {
-
+                    scope.launch {
+                        if(drawerState.isClosed){
+                            drawerState.open()
+                        }else{
+                            drawerState.close()
+                        }
+                    }
                 }
             )
         }
     ){ paddingValues ->
-        Box(
-            modifier = Modifier.fillMaxSize().padding(paddingValues),
-            contentAlignment = Alignment.Center
-        ){
-            Text(stringResource(R.string.hello_world))
-        }
+        ModalNavigationDrawer(
+            modifier = Modifier.padding(paddingValues),
+            drawerState = drawerState,
+            drawerContent = {
+                ModalDrawerSheet {
+                    Spacer(Modifier.height(12.dp))
+                    Text(stringResource(R.string.hello_from_nav_drawer))
+                }
+            },
+            content = {
+                Box(
+                    modifier = Modifier.fillMaxSize().padding(paddingValues),
+                    contentAlignment = Alignment.Center
+                ){
+                    Text(
+                        if(drawerState.isClosed){
+                            stringResource(R.string.swipe_to_open)
+                        }else{
+                            stringResource(R.string.swipe_to_close)
+                        }
+                    )
+                }
+            }
+        )
 
     }
 }
